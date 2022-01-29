f97a573caf1403e6d628726031b71bb0d9ba69a7
shikilnar <shikilnar@ya.ru>
2022-01-29 22:09:25 +0500

    refactor: function decode

--- src/index.js
+++ src/index.js
@@ -37,68 +37,35 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 const bit_table = {
-    '**********': ' ',
     '10': '.',
     '11': '-',
     '00': ''
   };
-  
+
 function decode(expr) {
-    const exLen = Math.floor(expr.length / 10);
-    //console.log('exLen: ', exLen);
+    const morseCharLength = 10;
     let bitChars=[];
-    for(let i = 0; i< exLen; i++){
-      let f = i*10;
-      let sub = expr.substr(f, 10);
-      //console.log(f, sub);
+    for(let i = 0; i < Math.floor(expr.length / morseCharLength); i++){
+      const sub = expr.substr(i * morseCharLength, morseCharLength);
       bitChars.push(sub);
     }
-    //console.log(bitChars);
-    // let morseChars = bitChars.reduce((p, c) => {
-    //   let res = '';
-    //   console.log('p: ', p, 'c: ', c);
-    //   if (c === '**********'){
-    //     res = '*';//bit_table['**********']
-    //   }
-    //   else {
   
-    //     for(let i = 0; i < 10; i = i + 2){
-    //       let s2 = c.substr(i, 2);
-    //       //console.log('c :', c, 'i: ', i, 's2: ', s2, 'bit_table[s2]: ', bit_table[s2]);
-    //       res += bit_table[s2];
-    //     }
-    //   }
-  
-    //   let m = MORSE_TABLE[res];
-    //   //console.log('res: ', res, 'p: ', p, 'm: ', m);
-    //   return p + res;
-    // });
-      let chars ='';
-     let morseChars = bitChars.forEach((c) => {
+    let chars ='';
+    bitChars.forEach((c) => {
       let res = '';
-      //console.log('c: ', c);
       if (c === '**********'){
-        res = bit_table['**********']
-      }
-      else {
-  
+        res = ''
+      } else {
         for(let i = 0; i < 10; i = i + 2){
-          let s2 = c.substr(i, 2);
-          //console.log('c :', c, 'i: ', i, 's2: ', s2, 'bit_table[s2]: ', bit_table[s2]);
-          res += bit_table[s2];
+          res += bit_table[c.substr(i, 2)];
         }
       }
-  
-      let m = MORSE_TABLE[res] ? MORSE_TABLE[res] : ' ';
-      // console.log('res: ', res,  'm: ', m);
-      return chars += m;
+      
+      let char = res !== '' ? MORSE_TABLE[res] : ' ';
+      return chars += char;
     });
   
-    //console.log(bitChars);
-    // console.log('chars: ',chars);
-  
-      //console.log(expr);
-      return chars;
+    return chars;
   }
 
 module.exports = {
